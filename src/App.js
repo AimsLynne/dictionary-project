68ed589b79bfaa7677f4977666072dbe157169a9
AimsLynne <130839244+AimsLynne@users.noreply.github.com>
2023-10-20 18:50:05 +0100

    Tested Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <header className="App-header">Hello👋</header>
+      <a href="#" className="btn btn-primary shadow">
+        Search
+      </a>
     </div>
   );
 }
